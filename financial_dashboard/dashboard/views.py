beba5d803153962544df71804a569ddeb6497ff1
Rafael <rafael.s.leitao@gmail.com>
2024-07-01 12:50:01 -0700

    Views, templates and forms created

--- financial_dashboard/dashboard/views.py
+++ financial_dashboard/dashboard/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
-# Create your views here.
+
+# View for the home page
+@login_required
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'dashboard/signup.html', {'form':form})
