c03f419d6ff5e877482dfe8a20c9c57631e32e17
Rafael <rafael.s.leitao@gmail.com>
2024-07-02 10:49:34 -0700

    Some css added to home, signup, login and logout

--- financial_dashboard/financial_dashboard/settings.py
+++ financial_dashboard/financial_dashboard/settings.py
@@ -17,6 +17,10 @@ import logging
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dashboard', 'static'),
+                    ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
